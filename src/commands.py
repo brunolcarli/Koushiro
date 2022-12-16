0f89053f82acf55f071cceaa4adc6a6fdff5966d
Bruno L. Carli <brunolcarli@gmail.com>
2022-12-16 20:13:42 -0300

    ADD simple average polarity command

--- src/commands.py
+++ src/commands.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from src.query import Query
 from koushiro.settings import __version__
 
 
@@ -25,3 +26,21 @@ async def version(ctx):
     Pings the bot to return its current version.
     """
     await ctx.send(f'Running version: {__version__}')
+
+
+@client.command(aliases=['avgp', 'avg_sent', 'avg_pol'])
+async def average_polarity(ctx):
+    """
+    Return the messages average polarities percentage.
+    """
+    try:
+        data = Query.query_message_avg_polarity()['data']['messageAveragePolarity']
+    except:
+        return await ctx.send('Sorry, something went wrong. Try again later!')
+
+    embed = discord.Embed(color=0x1E1E1E, type="rich")
+    embed.add_field(name='Positive :green_circle:', value=f"{data['positive']}%", inline=False)
+    embed.add_field(name='Negative :red_circle:', value=f"{data['negative']}%", inline=False)
+    embed.add_field(name='Neutral :white_circle: ', value=f"{data['neutral']}%", inline=False)
+
+    await ctx.send('Average sentiment polarities from messages', embed=embed)
